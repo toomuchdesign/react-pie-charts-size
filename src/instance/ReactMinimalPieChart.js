62d0d1c1ee0c718b9b46282abd10117ffe458bb1
Andrea Carraro <me@andreacarraro.it>
2020-04-28 09:53:22 +0200

    Add empty benchmark

--- src/instance/ReactMinimalPieChart.js
+++ src/instance/ReactMinimalPieChart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PieChart from "react-minimal-pie-chart";
 
+
 export default function ReactMinimalPieChart() {
   return React.createElement(PieChart, {
     data: [
